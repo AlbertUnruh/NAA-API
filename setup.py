46f790daa97529e09126dea4bc52b82b8ad78e5c
Albert Unruh <keenan.noack@gmail.com>
2021-09-16 09:22:52 +0200

    Update `setup.py`

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@ from distutils.core import setup
 
 setup(
     name='NAA',
-    version='',
     packages=['NAA'],
     url='https://github.com/AlbertUnruh/NAA-API',
     license='MIT',
     author='AlbertUnruh',
-    author_email='',
     description='The API for the NAA (NetworkAttachedAlbert) project'
 )
