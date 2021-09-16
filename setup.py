97189e3c89a4d151d68661a2719fdb1d9dc6a384
Albert Unruh <keenan.noack@gmail.com>
2021-09-16 09:43:14 +0200

    Update `setup.py`

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
 
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+
 setup(
     name='NAA',
     packages=['NAA'],
     url='https://github.com/AlbertUnruh/NAA-API',
     license='MIT',
     author='AlbertUnruh',
-    description='The API for the NAA (NetworkAttachedAlbert) project'
+    description='The API for the NAA (NetworkAttachedAlbert) project',
+    requires=requirements
 )
