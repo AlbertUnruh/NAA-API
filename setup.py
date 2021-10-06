a5debaa3b9f25b8d1626781684b5f7e87c048938
AlbertUnruh <keenan.noack@gmail.com>
2021-10-06 17:29:19 +0200

    Add version

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
+import re
 
 
 with open("requirements.txt") as f:
     requirements = f.readlines()
 
 
+with open("NAA/__init__.py") as f:
+    version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", f.read(), re.MULTILINE).group(1)
+
+
 setup(
     name='NAA',
+    version=version,
     packages=['NAA'],
     url='https://github.com/AlbertUnruh/NAA-API',
     license='MIT',
