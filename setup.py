743fce60c308c093c410c406001276c70dcc96ee
AlbertUnruh <keenan.noack@gmail.com>
2021-10-26 14:59:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,31 @@ from setuptools import setup
 import re
 
 
+with open("NAA/__init__.py") as f:
+    file = f.read()
+
+version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", file, re.MULTILINE).group(1)
+url = re.search(r"^__url__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", file, re.MULTILINE).group(1)
+license = re.search(r"^__license__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", file, re.MULTILINE).group(1)  # noqa
+author = re.search(r"^__author__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", file, re.MULTILINE).group(1)
+
+
 with open("requirements.txt") as f:
     requirements = f.readlines()
 
 
-with open("NAA/__init__.py") as f:
-    version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", f.read(), re.MULTILINE).group(1)
+name = "NAA"
+packages = [
+    f"{name}"
+]
 
 
 setup(
-    name='NAA',
+    name=name,
     version=version,
-    packages=['NAA'],
-    url='https://github.com/AlbertUnruh/NAA-API',
-    license='MIT',
-    author='AlbertUnruh',
-    description='The API for the NAA (NetworkAttachedAlbert) project',
+    packages=packages,
+    url=url,
+    license=license,
+    author=author,
     install_requires=requirements
 )
